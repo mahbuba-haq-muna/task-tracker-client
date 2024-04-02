91c25dc4ab05cd0e9b73e85524cc3718272765c5
muna <muna3828@gmail.com>
2024-04-02 22:26:21 +0600

    added card

--- src/assets/Component/Tasks/Tasks.jsx
+++ src/assets/Component/Tasks/Tasks.jsx
@@ -21,7 +21,13 @@ const Tasks = () => {
             </div>
             <div className="w-48 bg-purple-100">
                 <h3 className="bg-purple-500 p-2 rounded-t-md text-white text-center font-semibold">Pending</h3>
-                <p>task-1</p>
+                <div className="card-body">
+                        <h2 className="card-title">Task1</h2>
+                        <p></p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Complete</button>
+                        </div>
+                    </div>
             </div>
             <div className="w-48 bg-purple-100">
                 <h3 className="bg-purple-500 p-2 rounded-t-md text-white text-center font-semibold">Pending</h3>
